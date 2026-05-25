3c7bae1fbdb183682365a48a820fb6fda020209e
Jimbxxx <jimbxxx.content@gmail.com>
2026-05-25 12:08:20 +0100

    Update api.js

--- api.js
+++ api.js
@@ -1,1 +1,29 @@
+const API = CONFIG.API_BASE;
 
+// GET helpers
+async function getTeams() {
+  const res = await fetch(`${API}/teams`);
+  return res.json();
+}
+
+async function getDivisions() {
+  const res = await fetch(`${API}/divisions`);
+  return res.json();
+}
+
+async function getPlayers() {
+  const res = await fetch(`${API}/players`);
+  return res.json();
+}
+
+// USER PROFILE FETCH
+async function getPlayerByUsername(username) {
+  const res = await fetch(`${API}/players`);
+  const data = await res.json();
+
+  const players = data.data || {};
+
+  return Object.values(players).find(
+    p => p.username === username
+  );
+}
