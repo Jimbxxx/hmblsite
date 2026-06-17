d9892bdbaf4ac791855da756191dab22b475fe1e
Jimbxxx <jimbxxx.content@gmail.com>
2026-06-17 17:34:53 +0100

    Create admin-page.js

---
+++ admin-page.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", () => {
+  requireAdmin();
+  loadAdmin();
+});
+
+async function loadAdmin() {
+  const res = await fetch(`${API}/players`);
+  const data = await res.json();
+
+  const users = data.data || {};
+
+  let html = "<h2>Players</h2>";
+
+  Object.values(users).forEach(u => {
+    html += `
+      <div class="card">
+        <b>${u.username}</b><br>
+        Admin: ${u.is_admin ? "YES" : "NO"}
+      </div>
+    `;
+  });
+
+  document.getElementById("adminContent").innerHTML = html;
+}
+
+function goAdmin() {
+  window.location.href = "/admin.html";
+}
